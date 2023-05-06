a910e45c5b3c12b97ebf50341d73d74efb59ba29
Micael Ferreira <micaelmf@gmail.com>
2023-05-06 10:43:19 -0300

    implementing routes

--- new_track/aulas/02-api-rest/src/server.ts
+++ new_track/aulas/02-api-rest/src/server.ts
@@ -1,11 +1,22 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  return await knex('sqlite_schema').select('*')
+app.register(cookie)
+
+/**
+ * Um hook, que unciona como um middleware de escopo. Neste caso está no escopo global,
+ * mas também pode ser usdo no escopo das rotas (plugins)
+ */
+app.addHook('preHandler', async (request, reply) => {
+  console.log(`[${request.method}] ${request.url}`)
+})
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
